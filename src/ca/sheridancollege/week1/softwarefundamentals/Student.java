590a18919167e70ad22354af8fd9ff80b10bd290
Tiffany <truotiff@sheridancollege.ca>
2020-05-28 10:40:01 -0400

    PartTime - First Commit

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -9,28 +9,51 @@ package ca.sheridancollege.week1.softwarefundamentals;
  *
  * @author asidhu
  */
-public class Student {
-    
-    private String name;
+public class Student
+{
 
-    /**
-     * Get the value of name
-     *
-     * @return the value of name
-     */
-    public String getName() {
-        return name;
-    }
+   private String name;
 
-    /**
-     * Set the value of name
-     *
-     * @param name new value of name
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+   private String numHours;
+
+   /**
+    * Get the value of numHours
+    *
+    * @return the value of numHours
+    */
+   public String getNumHours ()
+   {
+      return numHours;
+   }
+
+   /**
+    * Set the value of numHours
+    *
+    * @param numHours new value of numHours
+    */
+   public void setNumHours (String numHours)
+   {
+      this.numHours = numHours;
+   }
+
+   /**
+    * Get the value of name
+    *
+    * @return the value of name
+    */
+   public String getName ()
+   {
+      return name;
+   }
+
+   /**
+    * Set the value of name
+    *
+    * @param name new value of name
+    */
+   public void setName (String name)
+   {
+      this.name = name;
+   }
 
 }
- 
-   
